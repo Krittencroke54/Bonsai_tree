e39e122cc25ba068eb1010bf8bd9cde99da2ac22
Krittencroke54 <RETROGRADE_RAINBOW@aol.com>
2016-01-15 17:26:42 -0500

    Changes made for attempt at new D.A.W.G

--- thenextone.js
+++ thenextone.js
@@ -1,6 +1,6 @@
 var html = '';
 
 for (var v = 1; v <= 100000000; v += 1) {
-    html += '<div>' + prompt("whatcha doing ya'll?") + '</div>'
+    html += '<div>' + var message = prompt("whatcha doing ya'll?") + '</div>'
 }
 document.write('div' + html + '/div');
