b6bd17c7d092defc302332895cb9453647e6b415
Joe_Dirt <ngoku97@gmail.com>
2016-02-09 03:10:39 -0500

    added some responsive function and adjusted css some

--- JS/energy_conversion.js
+++ JS/energy_conversion.js
@@ -214,6 +214,78 @@ var range;
 
 // var dist_coordinate = speed_of_light * seconds;
 
+ var frequency = 0;
+var seconds;
+var speed_of_light = 299792458;
+var wavelength_ft = 1100 / frequency;
+var wavelength_m = 335 / frequency;
+var meters = 0;
+
+var bass = 0;
+var midrange = 0;
+var seconds_to_degree = 0;
+var treble = 0;
+var degrees_of_cycle = 0;
+var	seconds_to_degree = frequency / ( degrees_of_cycle / 360 );
+var	pk_current = 1.414 * voltage;
+var 	pk_voltage = 1.414 * current;
+var	pk_root_mean_square = voltage * .707;
+var	pk_pk_root_mean_square = voltage * .354;
+var	time = 0;
+var frequency = frequency / seconds;
+var	cycle_degrees = 360;
+var	pi = 3.1416;
+var	radians = 2 * pi * frequency;
+var	power = voltage * current;
+var	power = (current * resistance) * resistance;
+var	power = (current * current ) * resistance;
+var	power = voltage * (voltage / resistance);
+var	power = ( voltage * voltage ) / resistance;
+var	voltage = current * resistance;
+var	current = voltage / resistance;
+var	resistance = voltage / current;
+var range;
+
+//int main() 
+//
+//	float power = 0, current = 0, voltage = 0, resistance = 0; 
+//char choice = 0;
+//
+//	var light_distance = speed_of_light * seconds;
+//    var speed_of_light / seconds = meters;
+//	wavelength_ft = 1100 / frequency;
+//	wavelength_m = 335 / frequency;
+ 
+//function frequency_conversion(frequency) { 
+//	if (frequency >=20 && frequency <= 200 )
+//		alert("frequency = bass");
+//	if else (frequency >= 200 && frequency <= 3000)
+//	alert("frequency = midrange");
+//	if else (frequency >= 3000)
+//	alert("frequency = treble");
+//    else 
+//        alert ("out of range");
+//        
+//        }
+//function frequency_conversion() {
+//    var frequency = document.getElementById("frequncy_conversions").value;
+//    // note: using the same variable for input AND output is not good practice
+//    if (frequency >= 20 && frequency <= 200) frequency = "bass";
+//    else if (frequency >= 200 && frequency <= 3000) frequency = "midrange";
+//    else if (frequency >= 3000) frequency = "treble";
+//    else range = "out of range";
+//    document.getElementById("range").innerHTML = frequency;
+//    alert('range of frequency is:' + frequency + '');
+//}
+//
+//    function distance_light() {
+//        var seconds = document.getElementById("light_coordinate").value
+// return  var dist_coordinate = speed_of_light * seconds;
+//        alert(dist_coordinate);
+//}
+
+// var dist_coordinate = speed_of_light * seconds;
+
 function frequency_conversion() {
   var frequency = document.getElementById("calculator").value;
   // note: using the same variable for input AND output is not good practice
@@ -239,6 +311,12 @@ function wavelength_to_frequency() {
     alert('The frequency with the given wavelength is:' + speed_of_light / wavlgnth + '');
     }
 
+function spdlight () {
+ var frequent = document.getElementById("frequet").value;
+  var wavlgnth = document.getElementById("wvlnth").value;
+   alert('the speed of light is: ' +  frequent * wavlgnth + ' meters per second.'); 
+}
+
 function powerCnR () {
  var current = document.getElementById("current").value;
   var resistance = document.getElementById("resistance").value;
@@ -249,6 +327,7 @@ function powerVnC () {
   var voltage = document.getElementById("volt").value;
   var current = document.getElementById("curre").value;
   alert('the power is ' + voltage * current + '');
+}
 }
 //h2{ input = frequency;
 //button = prompt("Your frequency range = + frequency_conversion +";
